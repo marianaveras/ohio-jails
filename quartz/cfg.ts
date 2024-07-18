049f89abc9a63e1c5cc15d39443cfbcaf479e06c
Mariana Veras <marianaveras@gmail.com>
2024-07-18 16:48:50 -0400

    Quartz sync: Jul 18, 2024, 4:48 PM

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -73,7 +73,7 @@ export interface FullPageLayout {
   header: QuartzComponent[]
   beforeBody: QuartzComponent[]
   pageBody: QuartzComponent
-  left: QuartzComponent[]
+  left: QuartzComponent[Explorer]
   right: QuartzComponent[]
   footer: QuartzComponent
 }
